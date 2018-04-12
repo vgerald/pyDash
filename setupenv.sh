483768561f12c9f4c74fab3f735ff884ba93e9ad
EC2 Default User <ec2-user@ip-172-31-93-163.ec2.internal>
2018-04-12 17:43:40 +0000

    added environment setup

---
+++ setupenv.sh
@@ -0,0 +1,17 @@
+# sudo yum git
+# git clone https://github.com/vgerald/pyDash.git
+
+sudo yum install python35 python35-virtualenv python35-pip
+#pip install --user pipenv
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+mkdir testDash
+cd testDash/
+virtualenv testDash
+virtualenv -p /usr/bin/python3 testDash
+##To begin using the virtual environment, it needs to be activated:
+source testDash/bin/activate
+activate
+pip install -r requirements.txt
+#rmvirtualenv venv
+#git remote add origin https://github.com/vgerald/pyDash.git
+#git push -u origin master

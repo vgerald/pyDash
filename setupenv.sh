0195a7cc86f41f47afe1b438463b6adf8971c0aa
EC2 Default User <ec2-user@ip-172-31-83-65.ec2.internal>
2018-04-13 03:57:52 +0000

    removed docker dependency

--- setupenv.sh
+++ setupenv.sh
@@ -1,11 +1,11 @@
 # sudo yum git
 # git clone https://github.com/vgerald/pyDash.git
 
-#yum install python35 python35-virtualenv python35-pip
+yum install python35 python35-virtualenv python35-pip
 #pip install --user pipenv
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-mkdir testDash
-cd testDash/
+#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+#mkdir testDash
+#cd testDash/
 virtualenv testDash
 virtualenv -p /usr/bin/python3 testDash
 ##To begin using the virtual environment, it needs to be activated:
